f2ee96adda5bcc9166fe2ede305c97f5a8525651
kingjiajie <303559363@qq.com>
2020-02-05 12:25:50 +0800

    添加配置信息类

---
+++ JJCollectionViewRoundFlowLayout_Swift/Classes/JJCollectionViewFlowLayoutConfig_Swift.swift
@@ -0,0 +1,13 @@
+//
+//  JJCollectionViewFlowLayoutConfig_Swift.swift
+//  JJCollectionViewRoundFlowLayout_Swift
+//
+//  Created by jiajie on 2020/1/19.
+//
+
+import UIKit
+
+public enum JJCollectionViewRoundFlowLayoutSwiftAlignmentType {
+    case System
+    case Lelt
+}
